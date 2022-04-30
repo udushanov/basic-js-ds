22bf7d67228ba96182853d247dfc75e2e75da6bd
Ulugbek <u.dushanov@gmail.com>
2022-04-30 18:24:38 +0500

    fix: queue task

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -16,18 +16,33 @@ const { NotImplementedError } = require('../extensions/index.js');
 class Queue {
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.queue
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    const node = new ListNode(value)
+
+    if (!this.queue) {
+      this.queue = node
+    } else {
+      let currentNode = this.queue
+
+      while(currentNode) {
+        if (!currentNode.next) {
+          currentNode.next = node
+          break
+        }
+
+        currentNode = currentNode.next
+      }
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const value = this.queue.value
+    this.queue = this.queue.next
+
+    return value
   }
 }
 
